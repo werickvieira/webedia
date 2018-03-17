75c6f802ed1239bd10bca5f209cb1e024794603c
Wérick Vieira <werick.vieira@yahoo.com.br>
2018-03-17 10:54:36 -0300

    Configurando o webpack

---
+++ webpack.config.babel.js
@@ -0,0 +1,39 @@
+import path from 'path';
+import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  entry: './app/src/js/app.js',
+  output: {
+    filename: 'bundle.js?[hash]',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader',
+        }],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin('style.css?[hash]'),
+  ],
+};
+
+export default config;
+
